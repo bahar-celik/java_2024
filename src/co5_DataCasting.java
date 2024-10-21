0d26836d762d8ee7777fd47de2ac138267458dcb
Bahar Çelik <baharcelik@Bahar-MacBook-Air.local>
2024-10-21 20:19:27 +0300

    first commit

---
+++ src/co5_DataCasting.java
@@ -0,0 +1,24 @@
+public class co5_DataCasting {
+    public static void main(String[] args) {
+
+        int sayiInt = 34;
+        String str = "Java Candır";
+        boolean bl = true;
+        char chr  = '%';
+        double dbl = 4.5;
+        byte byt = 10;
+         short shrt = 23;
+
+
+
+
+         // data casting
+        // boolean data turundeki bir variableye sadece boolean degerler atayabilirsiniz
+
+
+
+
+
+
+    }
+}
---
+++ src/co6_.java
@@ -0,0 +1,18 @@
+public class co6_ {
+    public static void main(String[] args) {
+
+        int sayi = 20;
+         byte byt = (byte)  sayi ;
+
+        System.out.println("20' nin byte olarak karşılığı : " + byt);
+
+        sayi = 256;
+        byt = (byte)sayi;
+        System.out.println("256'nın byte olarak karşılığı : " + byt);
+
+        sayi = 640;
+        byt= (byte)sayi;
+        System.out.println("640'ın byte olarak karşılığı :" + byt);
+
+    }
+}
